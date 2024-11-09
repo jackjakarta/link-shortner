4045af61fb6166f044827b623408c0222dca0da8
jackjakarta <al.termure@gmail.com>
2024-11-09 16:38:43 +0100

    feat: delete link

--- src/app/(app)/(user)/profile/[userId]/_components/actions.ts
+++ src/app/(app)/(user)/profile/[userId]/_components/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
+import { dbDeleteLink } from '@/db/functions/link';
 import { env } from '@/env';
+import { getUser } from '@/utils/auth';
 
 export async function generateQrCode({ linkId, url }: { linkId: string; url: string }) {
   const baseUrl = env.NEXT_PUBLIC_baseUrl;
@@ -18,4 +20,10 @@ export async function generateQrCode({ linkId, url }: { linkId: string; url: str
   if (!response.ok) {
     throw new Error(result.error);
   }
+}
+
+export async function deleteLink({ linkId }: { linkId: string }) {
+  const user = await getUser();
+
+  await dbDeleteLink({ linkId, userId: user.id });
 }
