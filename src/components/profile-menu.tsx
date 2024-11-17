739d31661eccd1eac8398e42b351e6e2bd10a6c7
Alex Termure <124639246+jackjakarta@users.noreply.github.com>
2024-11-17 23:07:44 +0100

    feat: add home profile dropdown (#34)

---
+++ src/components/profile-menu.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { type UserRow } from '@/db/schema';
+import { HelpCircle, Key, Link as LinkIcon, LogOut, User } from 'lucide-react';
+import { signOut } from 'next-auth/react';
+import Link from 'next/link';
+import { useState } from 'react';
+
+export default function ProfileMenu({ user }: { user: UserRow }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+      <DropdownMenuTrigger asChild>
+        <button className="flex items-center justify-center p-2 hover:bg-black/70 hover:text-white rounded-full group focus:outline-none">
+          <User className="text-black group-hover:text-white h-5 w-5" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">{user.name}</p>
+            <p className="text-xs leading-none text-muted-foreground">{user.email}</p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <Link href={`/profile/${user.id}/settings`}>
+          <DropdownMenuItem className="cursor-pointer">
+            <User className="mr-2 h-4 w-4" />
+            <span>Account</span>
+          </DropdownMenuItem>
+        </Link>
+        <Link href="/profile">
+          <DropdownMenuItem className="cursor-pointer">
+            <LinkIcon className="mr-2 h-4 w-4" />
+            <span>Your links</span>
+          </DropdownMenuItem>
+        </Link>
+        <Link href={`/profile/${user.id}/settings/api`}>
+          <DropdownMenuItem className="cursor-pointer">
+            <Key className="mr-2 h-4 w-4" />
+            <span>API Keys</span>
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuItem className="cursor-pointer">
+          <HelpCircle className="mr-2 h-4 w-4" />
+          <span>Help</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => void signOut({ callbackUrl: '/' })}
+          className="cursor-pointer"
+        >
+          <LogOut className="mr-2 h-4 w-4" />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
