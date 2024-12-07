25ba17ddfd49df1c02a09030ca503fdbc55e0446
jackjakarta <al.termure@gmail.com>
2024-12-07 17:48:21 +0100

    feat: style user action pages

--- src/app/user-action/email-verify-success.tsx
+++ src/app/user-action/email-verify-success.tsx
@@ -1,8 +1,23 @@
+import Link from 'next/link';
+
 export default function EmailVerifySuccess() {
   return (
-    <h1>
-      Ihre E-Mail wurde verifiziert. Sie können sich jetzt mit Ihrer E-Mail und Ihrem Passwort bei
-      der App anmelden.
-    </h1>
+    <div className="flex items-center justify-center min-h-screen bg-slate-900 dark:bg-gray-800 w-full px-4">
+      <div className="border-none rounded-full bg-gradient-to-r from-gray-700 to-indigo-900 p-8 shadow-lg">
+        <div className="text-center">
+          <h1 className="text-white text-2xl font-semibold mb-4">Email Verified Successfully!</h1>
+          <p className="text-gray-300 mb-4">
+            Your email has been verified. You can now log in to the app using your email and
+            password.
+          </p>
+          <Link
+            className="mt-6 px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+            href="/login"
+          >
+            Go to Login
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/app/user-action/token-verify-fail.tsx
+++ src/app/user-action/token-verify-fail.tsx
@@ -1,8 +1,23 @@
-export default function TokenVerifyFail() {
+import Link from 'next/link';
+
+export default function EmailVerifySuccess() {
   return (
-    <h1>
-      Dieser Link scheint ein Token zu enthalten, das defekt oder ungültig ist. Bitte fordern Sie
-      einen neuen an.
-    </h1>
+    <div className="flex items-center justify-center min-h-screen bg-slate-900 dark:bg-gray-800 w-full px-4">
+      <div className="border-none rounded-full bg-gradient-to-r from-gray-700 to-indigo-900 p-8 shadow-lg">
+        <div className="text-center">
+          <h1 className="text-white text-2xl font-semibold mb-4">Token Verification Failed</h1>
+          <p className="text-gray-300 mb-4">
+            This link appears to contain a token that is broken or invalid. Please request a new
+            one.
+          </p>
+          <Link
+            className="mt-6 px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-400"
+            href="/"
+          >
+            Go home
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
